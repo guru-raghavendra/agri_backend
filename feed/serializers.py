25fd5c0b6d4a6701e41fb58e3c6f14d019a3fc9b
Guru <hguru001@gmail.com>
2024-01-02 15:19:24 +0530

    changes

--- feed/serializers.py
+++ feed/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Comment, Post
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,8 +7,22 @@ class PostSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'content', 'image', 'video', 'like_count']
         read_only_fields = ['like_count', 'user']
     
-    def save(self, **kwargs):
-        user = kwargs.pop('user', None)
-        post = Post(**self.validated_data, user=user)
-        post.save()
-        return post
+    # def save(self, **kwargs):
+    #     user = kwargs.pop('user', None)
+    #     post = Post(**self.validated_data, user=user)
+    #     post.save()
+    #     return post
+    def create(self, validated_data):
+        # Access the user from the serializer context
+        user = self.context['request'].user
+        return Post.objects.create(user=user, **validated_data)
+    
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'post', 'parent_comment', 'content', 'like_count']
+        read_only_fields = ['user', 'like_count']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return Comment.objects.create(user=user, **validated_data)
--- feed/views.py
+++ feed/views.py
@@ -4,8 +4,8 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
 from authentication.authentication import TokenAuthentication
-from .models import Post
-from .serializers import PostSerializer
+from feed.models import Comment, Post
+from .serializers import CommentSerializer, PostSerializer
 
 class CreatePostView(APIView):
 
@@ -13,8 +13,47 @@ class CreatePostView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, format=None):
-        serializer = PostSerializer(data=request.data)
+        serializer = PostSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response({"message":"Success"}, status=status.HTTP_201_CREATED)
+            serializer.save()
+            return Response({"message": "Success"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LikePostView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, post_id, format=None):
+        post = Post.objects.get(id=post_id)
+        if request.user in post.liked_by.all():
+            post.liked_by.remove(request.user)
+            action = 'disliked'
+        else:
+            post.liked_by.add(request.user)
+            action = 'liked'
+        return Response({"message": f"Post {action}"}, status=status.HTTP_200_OK)
+
+class AddCommentView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, format=None):
+        serializer = CommentSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Comment added"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LikeCommentView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, comment_id, format=None):
+        comment = Comment.objects.get(id=comment_id)
+        if request.user in comment.liked_by.all():
+            comment.liked_by.remove(request.user)
+            action = 'disliked'
+        else:
+            comment.liked_by.add(request.user)
+            action = 'liked'
+        return Response({"message": f"Comment {action}"}, status=status.HTTP_200_OK)
